19ec263bcde5f7812368535dcdfeb916aa484b2f
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-04-07 10:53:29 +0100

    chore:small changes

---
+++ src/components/FloatingBar.tsx
@@ -0,0 +1,28 @@
+import { Moon, Sun, Github, Linkedin } from "lucide-react";
+import { useTheme } from "./";
+import { Theme } from '../types/theme';
+
+export default function FloatingBar() {
+  const { setTheme } = useTheme();
+
+  const handleThemeChange = (theme: Theme) => {
+    setTheme(theme);
+  };
+
+  return (
+    <div className="fixed bottom-10 left-4 right-4 flex justify-center">
+      <div className="border-2 w-44 flex items-center justify-around gap-4 px-2 py-3 rounded-md bg-white dark:bg-black shadow-lg transition-all hover:shadow-xl hover:scale-105">
+        
+        <button onClick={() => handleThemeChange('dark')}>
+          <Sun className="h-[1.2rem] w-[1.2rem] text-yellow-500 block dark:hidden" />
+        </button>
+        <button onClick={() => handleThemeChange('light')}>
+          <Moon className="h-[1.2rem] w-[1.2rem] text-gray-300 hidden dark:block" />
+        </button>
+
+        <Github className="h-[1.2rem] w-[1.2rem]" />
+        <Linkedin className="h-[1.2rem] w-[1.2rem]" />
+      </div>
+    </div>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { Linkedin } from 'lucide-react';
+import { Github } from 'lucide-react';
 
 const Header = () =>{
     
     return (
         <>
-        header
+        I'M Naboulsi
+        <motion.ul 
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ 
+                opacity: 1,
+                scale: 1,
+                transition: { duration: 0.3 }
+            }} 
+            className='flex w-20 items-center gap-4'
+        >
+            <li >
+                <Linkedin />
+            </li>
+            <li>
+                <Github />
+            </li>
+        </motion.ul>
         </>
     )
 }
