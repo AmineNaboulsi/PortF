ebb846d91d20f37d3c3dc2a27ff9d3b3401040de
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-04-07 09:26:24 +0100

    Upload PortFolio to github

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+function App() {
+
+  return (
+    <>
+      <Header />
+        <main>
+          
+        </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
