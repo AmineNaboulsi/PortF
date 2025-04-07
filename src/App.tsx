33c88bccd340c62e3ace5b431c4eba093080f59d
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-04-07 10:34:17 +0100

    feat: adding light and dark mode system with a floating bar bootm of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import FloatingBar from './components/FloatingBar'
+import { ThemeProvider } from "./components/theme-provider"
+
 function App() {
 
   return (
-    <>
+    <ThemeProvider>
       <Header />
         <main>
-          
         </main>
+          <FloatingBar />
       <Footer />
-    </>
+    </ThemeProvider>
   )
 }
 
